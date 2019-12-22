2db95bd8476d1b6af6f04c66da8ec6e78c2098e0
Vincent Moittié <vincent.moittie@arkena.com>
2019-12-22 13:51:06 +0100

    Create application constantes class

---
+++ src/models/Constants.js
@@ -0,0 +1,39 @@
+class Constants {
+  static get direction() {
+    return {
+      UP: 0,
+      DOWN: 1,
+      LEFT: 2,
+      RIGHT: 3,
+    };
+  }
+
+  static get screen() {
+    return {
+      WIDTH: 480,
+      HEIGHT: 480,
+    };
+  }
+
+  static get game() {
+    return {
+      block: {
+        WIDTH: 30,
+        HEIGHT: 30,
+      },
+      SPEED: 100,
+      image: {
+        SIZE: 16,
+      },
+    };
+  }
+
+  static get color() {
+    return {
+      BACKGROUND: '#BFCC00',
+      GRID: 0xDEE667,
+    };
+  }
+}
+
+export default Constants;

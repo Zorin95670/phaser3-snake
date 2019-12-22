1a1f2d5e9123022abe88ee94715a176fc7fb87bb
Vincent Moittié <vincent.moittie@arkena.com>
2019-12-22 13:51:22 +0100

    Create default food class

---
+++ src/models/Food.js
@@ -0,0 +1,25 @@
+import Phaser from 'phaser';
+import Constants from './Constants';
+
+const Food = new Phaser.Class({
+  Extends: Phaser.GameObjects.Image,
+
+  initialize(scene, x, y) {
+    Phaser.GameObjects.Image.call(this, scene);
+
+    this.setTexture('food');
+    this.setPosition(x * Constants.game.image.SIZE, y * Constants.game.image.SIZE);
+    this.setOrigin(0.5);
+
+    this.total = 0;
+    this.multiplicator = 1;
+    this.action = 'grow';
+
+    scene.children.add(this);
+  },
+  eating() {
+    this.total += 1;
+  },
+});
+
+export default Food;
